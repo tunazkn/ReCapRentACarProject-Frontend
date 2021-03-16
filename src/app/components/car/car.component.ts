c2ab0d2abc88124053064c0e896dd0ae24608499
Tuna ÖZKAN <75748254+tunazkn@users.noreply.github.com>
2021-03-16 15:07:57 +0300

    second

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Car } from 'src/app/models/car';
+import { HttpClient } from '@angular/common/http';
+import { CarResponseModel } from 'src/app/models/carResponseModel';
+import { SubjectSubscriber } from 'rxjs/internal/Subject';
 
 @Component({
   selector: 'app-car',
@@ -7,10 +10,19 @@ import { Car } from 'src/app/models/car';
   styleUrls: ['./car.component.css'],
 })
 export class CarComponent implements OnInit {
-  
   cars: Car[] = [];
+  apiUrl = 'https://localhost:44334/api/cars/getall';
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() {}
+  ngOnInit(): void {
+    this.getCars();
+  }
 
-  ngOnInit(): void {}
+  getCars() {
+    this.httpClient
+    .get<CarResponseModel>(this.apiUrl)
+    .subscribe((response) => {
+      this.cars = response.data;
+    });
+  }
 }
